8a23afb6fdd72f5468b0868fdc3db70463f28785
unknown <rom.c001@icloud.com>
2022-05-20 20:49:49 +0200

    Ajout lien git

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -3,12 +3,15 @@ import React from "react";
 const GitHub = () => {
     return (
         <div className="github">
+            <p>
+                Pour voir mes autres projets : <span></span>
+            </p>
             <a
                 href="https://romaincrz.github.io/"
                 target="_blank"
                 className="hover"
             >
-                <i className="fa-brands fa-github"></i>
+                <i className="fa-brands fa-github hover"></i>
             </a>
         </div>
     );
